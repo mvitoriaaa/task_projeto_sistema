2d5522b6c1f04b4e2b150f9f29cb875d26868a99
Maria Aparecida Vitoria Freitas Siqueira -201017 <maria.siqueira@fxnbr.com>
2026-05-26 15:51:00 -0400

    feature/ alterando coisas relacionadas a segurança

---
+++ src/main/java/com/example/taskapi/config/DataInitializer.java
@@ -0,0 +1,26 @@
+package com.example.taskapi.config;
+
+import com.example.taskapi.model.Usuario;
+import com.example.taskapi.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initAdmin(UsuarioRepository repo, PasswordEncoder encoder) {
+        return args -> {
+            if (repo.findByUsername("admin").isEmpty()) {
+                repo.save(new Usuario(null, "admin", encoder.encode("admin")));
+                System.out.println("[Init] Usuário 'admin' criado no banco com senha hash BCrypt.");
+            } else {
+                System.out.println("[Init] Usuário 'admin' já existe no banco.");
+            }
+        };
+    }
+}
